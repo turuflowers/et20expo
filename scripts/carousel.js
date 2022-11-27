640a4b6941e2119687c070ef17c38ddf3481b412
turuflowers@gmail.com <M4v3r1ckbkr221b>
2022-11-27 19:54:13 -0300

    html desktop

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -8,17 +8,12 @@ $(document).ready(function(){
       imagesLoaded: true,
       prevNextButtons: true,
       pageDots: true,
-      wrapAround: true,
+      wrapAround: false,
     groupCells: true,
       fade: true,
       autoPlay: 5000,
       pauseAutoPlayOnHover: true,
-      arrowShape: {
-          x0: 10,
-          x1: 60, y1: 50,
-          x2: 65, y2: 45,
-          x3: 20
-      }  
+    arrowShape: ''
   });
 });
     
