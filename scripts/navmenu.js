1b5f5b03be3211b415b9e72ecb534088980ac9da
Nicolás Flores <turuflowers@gmail.com>
2022-11-27 17:09:13 +0000

    base

---
+++ scripts/navmenu.js
@@ -0,0 +1,39 @@
+$(window).resize(function() {
+    var w = $(window).width();
+    if(w > 991){
+        $("header").removeClass('active');
+        $('.hamburger').removeClass('active');
+        $(".navegacion").removeClass('active');
+        $(".subnav").removeClass('active');
+    }
+});
+$(document).ready(function () {
+  $('.hamburger').on('click', function () {
+      $("header").toggleClass('active');
+      $(this).toggleClass('active');
+      $(".navegacion").toggleClass('active');
+      $(".subnav").removeClass('active');
+  });
+
+  $('nav a').on('click', function(){
+      $("header").removeClass('active');
+      $('.hamburger').removeClass('active');
+      $(".navegacion").removeClass('active');
+      $(".subnav").removeClass('active');
+  });
+
+  $('nav button').on('click', function(){
+    var subnav = $(this).next();
+    if(subnav.hasClass('active')){
+      subnav.removeClass('active');
+      
+    }else{
+      $('.subnav').removeClass('active');
+      subnav.addClass('active');
+    }
+  });
+  $('nav .volver').on('click', function(){
+    var subnav = $(this).parent();
+    subnav.removeClass('active');
+  });
+});
