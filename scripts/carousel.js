ca7b6bca08e2ae1fe4028ee39ae19f8ddc733f6d
Nicolas Flores <turuflowers@gmail.com>
2022-11-28 07:46:20 -0300

    proyectos base

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -32,5 +32,23 @@ $(document).ready(function(){
     pauseAutoPlayOnHover: true,
     arrowShape: ''
   });
+
+$('#carousel_proyectos').flickity({
+    // options
+    cellAlign: 'center',
+    contain: false,
+    draggable: true,
+    adaptiveHeight: true,
+    imagesLoaded: true,
+    prevNextButtons: true,
+    pageDots: true,
+    wrapAround: false,
+    groupCells: true,
+    fade: true,
+    autoPlay: 5000,
+    pauseAutoPlayOnHover: true,
+    arrowShape: ''
+});
+
 });
     
